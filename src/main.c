2d5c57104b68ca79a2aca9efeecc4fb22f2173a4
Stefan Hladnik <stefan@notes.rupa.lan>
2012-02-29 21:12:24 +0100

    Call keys_init() and some cleanup.

--- src/main.c
+++ src/main.c
@@ -13,22 +13,17 @@ int main()
 {
   timer_init();
   sei();
-
   clock_init();
-
 #ifndef NDEBUG
   print_buf_init();
 #endif
   uart_init();
-
   relay_off_all();
   valve_init();
-
   lcd_init();
-
   loops_start();
-
   watchdog_start();
+  keys_init();
 
   while (1) {
     sch();
