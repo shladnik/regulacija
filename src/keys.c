4354fb2822d80bc621b11d17b0c402726213451b
Stefan Hladnik <stefan@notes.rupa.lan>
2012-04-05 06:50:24 +0200

    Update.

--- src/keys.c
+++ src/keys.c
@@ -1,46 +1,65 @@
-#define ISC_LOW     ((0 << ISC01) | (0 << ISC00))
-#define ISC_EDGE    ((0 << ISC01) | (1 << ISC00))
-#define ISC_NEGEDGE ((1 << ISC01) | (0 << ISC00))
-#define ISC_POSEDGE ((1 << ISC01) | (1 << ISC00))
+#define ISC_LOW     (((0 << ISC01) | (0 << ISC00)) >> ISC00)
+#define ISC_EDGE    (((0 << ISC01) | (1 << ISC00)) >> ISC00)
+#define ISC_NEGEDGE (((1 << ISC01) | (0 << ISC00)) >> ISC00)
+#define ISC_POSEDGE (((1 << ISC01) | (1 << ISC00)) >> ISC00)
+#define ISC_MASK    ISC_POSEDGE
+
+static const timer_t t_settle = TIMER_MS(20);
+
+typedef struct {
+  func_t press;
+  func_t release;
+} key_t;
+
+key_t keys [1];
 
 void keys_init()
 {
   MCUCR |= (ISC_NEGEDGE << ISC10) | (ISC_NEGEDGE << ISC00);
-  GICR  |= (1 << INT1) | (1 << INT0);
+  GIFR   = (1 << INTF1) | (1 << INTF0);
+  GICR  |= (1 << INT1 ) | (1 << INT0 );
+}
+
+static bool int_edge_get(uint8_t int_nr)
+{
+  const uint8_t shift = int_nr ? ISC10 : ISC00;
+  return ((MCUCR >> shift) & ISC_MASK) == ISC_POSEDGE;
+}
+
+static void int_edge_set(uint8_t int_nr, bool edge)
+{
+  const uint8_t shift = int_nr ? ISC10 : ISC00;
+  MCUCR &= ~(ISC_MASK << shift);
+  MCUCR |= (edge ? ISC_POSEDGE : ISC_NEGEDGE) << shift;
 }
 
 void int0_cb()
 {
-  if (!port_get_pin(3, 2)) {
-    DBG static uint8_t int0_cnt;
-    int0_cnt++;
-  }
   GIFR  = 1 << INTF0;
+
+  bool edge = int_edge_get(0);
+  if (port_get_pin(3, 2) == edge) {
+    if (edge) DBG_CNT(int0_posedge);
+    else      DBG_CNT(int0_negedge);
+    int_edge_set(0, !edge);
+  } else {
+    if (edge) DBG_CNT(int0_posedge_false);
+    else      DBG_CNT(int0_negedge_false);
+  }
+
   GICR |= 1 << INT0;
 }
 
 DBG_ISR(INT0_vect)
 {
-  if (!port_get_pin(3, 2)) {
+  if (port_get_pin(3, 2) == int_edge_get(0)) {
     GICR &= ~(1 << INT0);
-    timer_add(TIMER_MS(25), int0_cb, 0, -1);
+    if (t_settle) timer_add(t_settle, int0_cb, 0, -1);
+    else          int0_cb();
   }
-}
-
-void int1_cb()
-{
-  if (!port_get_pin(3, 3)) {
-    DBG static uint8_t int1_cnt;
-    int1_cnt++;
-  }
-  GIFR  = 1 << INTF1;
-  GICR |= 1 << INT1;
 }
 
 DBG_ISR(INT1_vect)
 {
-  if (!port_get_pin(3, 3)) {
-    GICR &= ~(1 << INT1);
-    timer_add(TIMER_MS(50), int1_cb, 0, -1);
-  }
+  DBG_CNT(int1_posedge);
 }
