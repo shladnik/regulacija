78a1081c6dbf3b10dc6f3d8f33f75ff706f820e4
Stefan Hladnik <stefan.hladnik@gmail.com>
2015-03-01 10:41:35 -0800

    Few more safety measures when writting flash.
    Initialize check as soon as possible to reduce the amount of
    instructions where random crash could end up causing random write.
    Also assert() instead of only "if" when range seem invalid to stop
    further execution.

--- src/flash.c
+++ src/flash.c
@@ -26,61 +26,58 @@ bool writeallow_range(uintptr_t adr, uintptr_t len)
 }
 
 __attribute__((section(".flash_write")))
-void flash_write_block(uint8_t * buf, uintptr_t adr, uintptr_t len)
+void flash_write_block(uint8_t * buf, uintptr_t adr, uintptr_t len, uint8_t check)
 {
-  volatile uint8_t check;
-  check = BOOT_SPM_CHECK_VAL;
+  assert(config_range(adr, len) || writeallow_range(adr, len));
 
-  if (config_range(adr, len) || writeallow_range(adr, len)) {
-    uintptr_t start = adr;
-    uintptr_t end   = adr + len;
-    uintptr_t i = ~(SPM_PAGESIZE - 1) & start;
-    uintptr_t e = i + SPM_PAGESIZE;
-    uintptr_t start_a = start & ~0x1;
-    uintptr_t end_a   = end   & ~0x1;
-  
-    DBG_ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-      eeprom_busy_wait();
-      boot_spm_busy_wait();
-  
-      bool change = 0;
-  
-      for (; i < start_a; i += 2) boot_page_fill(i, pgm_read_word(i));
-      if (start & 0x1) {
-        uint16_t dat_old = pgm_read_word(i);
-        uint16_t dat = (dat_old & 0x00ff) | (*buf << 8);
-        change |= dat != dat_old;
-        buf++;
-        boot_page_fill(i, dat);
-        i += 2;
-      }
-      for (; i < end_a; i += 2) {
-        uint16_t dat_old = pgm_read_word(i);
-        uint16_t dat = *buf;
-        buf++;
-        dat |= *buf << 8;
-        buf++;
-        change |= dat != dat_old;
-        boot_page_fill(i, dat);
-      }
-      if (end & 0x1) {
-        uint16_t dat_old = pgm_read_word(i);
-        uint16_t dat = (dat_old & 0xff00) | *buf;
-        change |= dat != dat_old;
-        buf++;
-        boot_page_fill(i, dat);
-        i += 2;
-      }
-      for (; i < e; i += 2) boot_page_fill(i, pgm_read_word(i));
-      
-      if (change) {
-        assert(boot_page_erase_checked(adr, check) == 0);
-        boot_spm_busy_wait();
-        assert(boot_page_write_checked(adr, check) == 0);
-        boot_spm_busy_wait();
-      }
-      boot_rww_enable();
+  uintptr_t start = adr;
+  uintptr_t end   = adr + len;
+  uintptr_t i = ~(SPM_PAGESIZE - 1) & start;
+  uintptr_t e = i + SPM_PAGESIZE;
+  uintptr_t start_a = start & ~0x1;
+  uintptr_t end_a   = end   & ~0x1;
+
+  DBG_ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    eeprom_busy_wait();
+    boot_spm_busy_wait();
+
+    bool change = 0;
+
+    for (; i < start_a; i += 2) boot_page_fill(i, pgm_read_word(i));
+    if (start & 0x1) {
+      uint16_t dat_old = pgm_read_word(i);
+      uint16_t dat = (dat_old & 0x00ff) | (*buf << 8);
+      change |= dat != dat_old;
+      buf++;
+      boot_page_fill(i, dat);
+      i += 2;
     }
+    for (; i < end_a; i += 2) {
+      uint16_t dat_old = pgm_read_word(i);
+      uint16_t dat = *buf;
+      buf++;
+      dat |= *buf << 8;
+      buf++;
+      change |= dat != dat_old;
+      boot_page_fill(i, dat);
+    }
+    if (end & 0x1) {
+      uint16_t dat_old = pgm_read_word(i);
+      uint16_t dat = (dat_old & 0xff00) | *buf;
+      change |= dat != dat_old;
+      buf++;
+      boot_page_fill(i, dat);
+      i += 2;
+    }
+    for (; i < e; i += 2) boot_page_fill(i, pgm_read_word(i));
+    
+    if (change) {
+      assert(boot_page_erase_checked(adr, check) == 0);
+      boot_spm_busy_wait();
+      assert(boot_page_write_checked(adr, check) == 0);
+      boot_spm_busy_wait();
+    }
+    boot_rww_enable();
   }
 }
 
@@ -89,30 +86,33 @@ __attribute__((used)) NOINIT uint8_t flash_buf [SPM_PAGESIZE]; // for external u
 
 USED void flash_write(uint8_t * buf, uintptr_t adr, uintptr_t len)
 {
+  volatile uint8_t check = BOOT_SPM_CHECK_VAL;
+
   uintptr_t start_floor =  adr        &  (SPM_PAGESIZE - 1);
   uintptr_t end_floor   = (adr + len) & ~(SPM_PAGESIZE - 1);
   
   if (start_floor) {
     uintptr_t len_s = MIN(len, SPM_PAGESIZE - start_floor);
-    flash_write_block(buf, adr, len_s);
+    flash_write_block(buf, adr, len_s, check);
     buf += len_s;
     adr += len_s;
     len -= len_s;
   }
   
   while (adr < end_floor) {
-    flash_write_block(buf, adr, SPM_PAGESIZE);
+    flash_write_block(buf, adr, SPM_PAGESIZE, check);
     buf += SPM_PAGESIZE;
     adr += SPM_PAGESIZE;
     len -= SPM_PAGESIZE;
   }
 
   if (len) {
-    flash_write_block(buf, adr, len);
+    flash_write_block(buf, adr, len, check);
     buf += len;
     adr += len;
     len -= len;
   }
+  check = 0;
 }
 
 USED void flash_read(uint8_t * buf, uintptr_t adr, uintptr_t len)
--- src/flash.h
+++ src/flash.h
@@ -1,7 +1,6 @@
 #ifndef __FLASH_H__
 #define __FLASH_H__
 
-void flash_write_block(uint8_t * buf, uintptr_t adr, uintptr_t len);
 void flash_write(uint8_t * buf, uintptr_t adr, uintptr_t len);
 void flash_read (uint8_t * buf, uintptr_t adr, uintptr_t len);
 
